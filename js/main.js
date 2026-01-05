f82d4576e28d62d41491d6496aa2302a2393e082
glubtubis-wepal <zbooker000@stu.waukesha.k12.wi.us>
2026-01-05 14:25:24 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-// Initialize canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Main loop
 function loop() {
-  // Update
   kart.update();
 
-  // Draw
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   track.draw(ctx);
   kart.draw(ctx);
