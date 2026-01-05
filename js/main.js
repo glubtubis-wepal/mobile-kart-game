cc26768b5aee29fd022f7e0a5823a57285da0086
glubtubis-wepal <zbooker000@stu.waukesha.k12.wi.us>
2026-01-05 14:29:14 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// Camera offset
+let camX = 0;
+let camY = 0;
+
 function loop() {
   kart.update();
 
+  // Camera follows kart
+  camX = kart.x - canvas.width / 2;
+  camY = kart.y - canvas.height / 2;
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.save();
+  // Move the canvas origin opposite to camera
+  ctx.translate(-camX, -camY);
+
   track.draw(ctx);
   kart.draw(ctx);
+
+  ctx.restore();
 
   requestAnimationFrame(loop);
 }
